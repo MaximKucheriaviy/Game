b92c68981fccf6d54ffeac7383070012e28633bd
Maxim Kucheriaviy <maxim-k@i.ua>
2022-11-11 08:01:23 +0200

    ok push

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,17 +1,44 @@
 import { GameStyled } from "./Game.styled";
 import { Sprite } from "../Sprite/Sprite";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
+
+const tick = new Event("tick")
 
 export const Game = () => {
-    const [val, setVal] = useState(100);
+    const isStarted = useRef(false);
+    const [spriteMoveSpeed,setSpriteMoveSpeed] = useState(0);
+    const keyDown =  e =>{
+        console.log(e.key);
+        if(e.key === "a"){
+            setSpriteMoveSpeed(-2)
+        }
+        else if(e.key === "d"){
+            setSpriteMoveSpeed(2)
+        }
+    };
+    const keyUp =  e =>{
+        if(e.key === "a"){
+            setSpriteMoveSpeed(0)
+        }
+        else if(e.key === "d"){
+            setSpriteMoveSpeed(0)
+        }
+    };
     useEffect(() => {
+        if(isStarted.current){
+            return;
+        }
+        isStarted.current = true;
         setInterval(() => {
-            setVal(Math.random());
-        }, 30);
+            document.dispatchEvent(tick);
+        }, 1000);
+        document.addEventListener("keypress", keyDown);
+        document.addEventListener("keyup", keyUp);
     }, []);
+
     
-    return <GameStyled val={val} width={300} height={600}>
-        <Sprite width={50} height={30} x={10} y={40}/>
+    return <GameStyled width={300} height={600}>
+        <Sprite width={50} height={30} x={10} y={40} moveSpeed={spriteMoveSpeed}/>
     </GameStyled>
 }
--- src/components/Sprite/Sprite.jsx
+++ src/components/Sprite/Sprite.jsx
@@ -1,16 +1,39 @@
 import styled from "styled-components"
+import { useEffect, useState, useRef } from "react"
 
 const SpriteStyled = styled.div`
     position: absolute;
     width: ${props => props.width}px;
     height: ${props => props.height}px;
-    top: ${props => props.y}px;
-    left: ${props => props.x}px;
+    top: 0px;
+    left: 0px;
+
     background-color: red;
 `
 
-export const Sprite = ({width, height, x, y}) => {
-    return <SpriteStyled width={width} height={height} x={x} y={y}>
+export const Sprite = ({width, height, x, y, moveSpeed}) => {
+    const [widthState] = useState(width);
+    const [heightState] = useState(height);
+    const [xState, setX] = useState(x);
+    const [yState] = useState(y);
+    const prevUpdate = useRef(Date.now());
+    const suspenser = 10;
+
+    useEffect(() => {
+        setInterval(() => {
+            const delay = Date.now() - prevUpdate.current;
+            prevUpdate.current = Date.now();
+            setX(prev => {
+                if(delay === 0){
+                    return prev;
+                }
+                return prev + (Math.ceil(moveSpeed * delay / suspenser));
+            });
+        }, 10);
+    }, [moveSpeed])
+    
+    
+    return <SpriteStyled style={{transform: `translate(${xState}px, ${yState}px)`}} width={widthState} height={heightState} x={xState} y={yState}>
 
     </SpriteStyled>
 } 
